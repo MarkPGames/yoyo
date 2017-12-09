d8a51a841cb3c452ddaa7c4715b91d6c47e21441
Steadz <markelvisphillips@gmail.com>
2017-12-09 15:58:18 +1100

    Rotated "YoYo" sprite using the component HngeJoin2D's Motor function Added Movement script

---
+++ YoYo/Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    bool expand;
+    bool shrink;
+    bool pressed;
+    Vector3 shrinkTargetPos;
+    Vector3 expandTargetPos;
+    // Use this for initialization
+    void Start ()
+    {
+        expand = false;
+        shrink = true;
+        pressed = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+
+    }
+}
